815c912259e7b95cb1dcd33290580239ffb6501d
Khalil Hafni <khafni@e1r1p4.1337.ma>
2020-01-18 17:08:22 +0100

    grwrwgwr

--- data_intrepretation/print_string.c
+++ data_intrepretation/print_string.c
@@ -4,31 +4,57 @@
 
 
 
+void print_n_space(int n, int size)
+{
+    while (n && size < )
+    {
+        ft_putchar_fd(' ', 1);
+        n--;
+    }
+}
+
+int ft_strnlen(char *str, int n)
+{
+    int i;
+
+	i = 0;
+	while (str[i] && i < n)
+	{
+		i++;
+	}
+	return (i);
+}
+
 void print_string (char *fstr, va_list alist)
 {
     t_format *holder;
+    int ls;
     int l;
+    int fw;
 
-    l = ft_strlen(fstr);
     holder = get_data(fstr, alist);
-
-    if (!holder->precision)
-    {
-        if (l > holder->field_width)
-            ft_putstr_fd(fstr, 1);
-    }
+    fw = holder->field_width;
+    ls = ft_strnlen(fstr, holder->precision);
+    l = ft_strlen(fstr);
+    if (!holder->minus)
+        {
+            print_n_space(holder->field_width - 1, );
+            ft_putstr_fd(va_arg(alist, char *), 1);
+        }
+    
 }
 
 void ft_printf(char *str, ...)
 {
     va_list ab;
     va_start(ab, str);
-    print_char(str, ab);
+    print_string(str, ab);
+    va_end(ab);
    
 }
 
 int main()
 {
-    printf("%---5s", "brw");
+    ft_printf("%20s", "momo");
     return (0);
 }
