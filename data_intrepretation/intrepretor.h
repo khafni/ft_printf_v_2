5f694a3022cc3a59272d2b62297007591a7ee6d9
khafni <khafni@student.1337.ma>
2020-01-23 00:16:13 +0100

    :)

--- data_intrepretation/intrepretor.h
+++ data_intrepretation/intrepretor.h
@@ -6,3 +6,10 @@ typedef struct
     int minus;
     int left;
 } t_result;
+
+/*
+** prototypes for the different functions
+*/
+
+t_result *result_init(void);
+void zeros_calculator(t_format *holder, t_result *result, int size);
--- data_reading/data_read.c
+++ data_reading/data_read.c
@@ -1,10 +1,18 @@
 #include "../data_gathering_abstraction/data_gathering.h"
 #include "../data_intrepretation/intrepretor.h"
+#include "../libft/libft.h"
 void parser(char *str, va_list alist)
 {
     while (*str)
     {
+        if (*str = '%')
+        {
         
+        }
+        else
+        {
+            ft_putchar_fd(*str, 1);
+        }
         str++;
     }
 }
@@ -14,4 +22,10 @@ void ft_printf(char *str, ...)
     va_start(alist, str);
     parser(str, alist);
     va_end(alist);
+}
+
+int main()
+{
+
+    return (0);
 }
